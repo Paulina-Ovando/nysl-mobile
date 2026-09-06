b336ef6c7ddb5b30323ebb257aa6094fead4d7cb
Paulina-Ovando <paulinasanchez67@aragon.unam.mx>
2026-09-06 14:14:29 -0600

    Update starter code with NYSL text

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Northside Youth Soccer League</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to the NYSL Mobile App! 
+          Stay up to date with game schedules, locations, and team chats.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
